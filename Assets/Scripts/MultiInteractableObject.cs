8c1a3b2c8efe478d2b9b68dd187e468c63b5b777
Mrpotato-07 <love.helgesson@elev.ga.lbs.se>
2024-01-11 01:38:18 +0100

    e
    e

---
+++ Assets/Scripts/MultiInteractableObject.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HidingInteractables : Collidables
+{
+    private bool z_interacted = false;
+    protected override void OnCollided(GameObject collidedObject)
+    {
+        if (Input.GetKey(KeyCode.E))
+        {
+            OnInteract();
+
+        }
+
+    }
+
+
+
+    private void OnInteract()
+    {
+        if (!z_interacted)
+        {
+            z_interacted = true;
+            Debug.Log("(2) Interakterade med  " + name);
+        }
+        z_interacted = false;
+    }
+}
