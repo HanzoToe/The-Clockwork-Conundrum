b3990a9af4068e026f08a0eeac5e76a6d7dda55f
Dani-Deez <daniel.antunesgoncalves@elev.ga.lbs.se>
2024-01-13 15:40:14 +0100

    Eeeeeeeeeeeeee

---
+++ Assets/Scripts/PopUp_SCI/Phone/E_Phone.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class E_Phone : MonoBehaviour
+{
+    public GameObject PopUp_E;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        PopUp_E.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            PopUp_E.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Scripts/PopUp_SCI/Phone/Space_Phone.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Space_Phone : MonoBehaviour
+{
+    public GameObject PopUp_Space;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            PopUp_Space.SetActive(true);
+        }
+    }
+}
