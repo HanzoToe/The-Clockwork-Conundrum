3b03a3bcbb7a013efc241531586c2a085332a09d
Darren Scott <darrenpipi8@gmail.com>
2024-02-09 12:55:18 +0100

    game finished

---
+++ Assets/credits.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class credits : MonoBehaviour
+{
+    public GameObject button;
+    public GameObject button2;
+    public float cooldown = 22f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+         
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        cooldown -= Time.deltaTime;
+
+        if (cooldown <= 0)
+        {
+            button.SetActive(true);
+            button2.SetActive(true);
+        }
+    }
+
+   public void quitgame()
+    {
+        Application.Quit();
+    }
+
+   public void MainMenu()
+    {
+        SceneManager.LoadScene("Main_Menu");
+    }
+
+
+}
